1f0eb6081c762ecf420a2ae3f7d08733b8fb5911
18298459311@163.com <satanail001>
2020-03-31 21:33:11 +0800

    first commit

---
+++ src/main/java/com/lagou/sqlSession/Executor.java
@@ -0,0 +1,15 @@
+package com.lagou.sqlSession;
+
+import com.lagou.pojo.Configuration;
+import com.lagou.pojo.MappedStatement;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface Executor {
+
+    public <E> List<E> query(Configuration configuration,MappedStatement mappedStatement,Object... params) throws Exception;
+
+    void update(Configuration configuration,MappedStatement mappedStatement,Object object) throws SQLException, ClassNotFoundException, Exception;
+
+}
